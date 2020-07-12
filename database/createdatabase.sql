b7e7dfe02f0ab977284065819e10bb8b1bab689b
Wolanczyk <pawel.wolanczyk@solarwinds.com>
2020-07-12 20:45:05 +0200

    initial database creation script

--- database/createdatabase.sql
+++ database/createdatabase.sql
@@ -1,2 +1,43 @@
-CREATE TABLE users (user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name NVARCHAR NOT NULL, first_name NVARCHAR, last_name NVARCHAR);
+CREATE TABLE users(
+	user_id INT NOT NULL AUTO_INCREMENT, 
+	name NVARCHAR NOT NULL, 
+	first_name NVARCHAR, 
+	last_name NVARCHAR, 
+	PRIMARY KEY (user_id)
+);
 
+CREATE TABLE account_types(
+	account_type_id INT NOT NULL AUTO_INCREMENT, 
+	account_type NVARCHAR,
+	PRIMARY KEY (account_type_id)
+);
+
+CREATE TABLE accounts(
+	account_id INT NOT NULL AUTO_INCREMENT, 
+	user_id INT, 
+	account_type_id INT, 
+	username NVARCHAR, 
+	password NVARCHAR, 
+	creation_date DATETIME, 
+	PRIMARY KEY (account_id),
+	FOREIGN KEY (user_id) REFERENCES users (user_id),
+	FOREIGN KEY (account_type_id) REFERENCES account_types (account_type_id)
+);
+
+CREATE TABLE recreation_day(
+	recreation_day_id INT NOT NULL AUTO_INCREMENT,
+	start_time DATETIME,
+	end_time DATETIME,
+	PRIMARY KEY (recreation_day_id)
+);
+
+CREATE TABLE reservation(
+	reservation_id INT NOT NULL AUTO_INCREMENT,
+	recreation_day_id INT,
+	account_id INT,
+	PRIMARY KEY (reservation_id),
+	FOREIGN KEY (recreation_day_id) REFERENCES recreation_day (recreation_day_id),
+	FOREIGN KEY (account_id) REFERENCES accounts (account_id)
+);
+
+INSERT INTO account_types (account_type) VALUES('administrator'), ('rw_user'), ('ro_user');

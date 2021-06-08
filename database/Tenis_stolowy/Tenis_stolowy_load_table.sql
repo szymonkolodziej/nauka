d8c18845b3dac461d1b1ff7cdf9d1d8d976983aa
szymon kolodziej <szymonk474@gmail.com>
2021-06-08 13:02:00 +0200

    Inserty - Tenis_stolowy_load_table

---
+++ database/Tenis_stolowy/Tenis_stolowy_load_table.sql
@@ -0,0 +1,70 @@
+INSERT INTO Deski values ('Andro', 'Gauzy BL7 OFF', 71, 134)
+
+
+SELECT * FROM Okładziny
+insert into Okładziny values ('butterfly', 'Rozena', 2.1, 90, 450)
+insert into Okładziny values ('butterfly', 'Rozena', 1.9, 85, 450)
+insert into Okładziny values ('butterfly', 'Tenergy 05', 1.9, 85, 260)
+insert into Okładziny values ('butterfly', 'Tenergy 05', 2.0, 88, 260)
+insert into Okładziny values ('butterfly', 'Tenergy 05', 2.1, 91, 260)
+insert into Okładziny values ('butterfly', 'Tenergy 64', 1.9, 86, 270)
+insert into Okładziny values ('butterfly', 'Tenergy 64', 2.0, 89, 270)
+insert into Okładziny values ('butterfly', 'Tenergy 64', 2.1, 92, 270)
+insert into Okładziny values ('butterfly', 'Tenergy 80', 1.9, 85, 300)
+insert into Okładziny values ('butterfly', 'Tenergy 80', 2.0, 87, 300)
+insert into Okładziny values ('butterfly', 'Tenergy 80', 2.1, 89, 300)
+insert into Okładziny values ('butterfly', 'Tenergy 25', 1.9, 87, 290)
+insert into Okładziny values ('butterfly', 'Tenergy 25', 2.0, 88, 290)
+insert into Okładziny values ('butterfly', 'Tenergy 25', 2.1, 89, 290)
+insert into Okładziny values ('butterfly', 'Tenergy 19', 1.9, 87, 275)
+insert into Okładziny values ('butterfly', 'Tenergy 19', 2.0, 88, 275)
+insert into Okładziny values ('butterfly', 'Tenergy 19', 2.1, 89, 275)
+insert into Okładziny values ('butterfly', 'Sriver', 1.9, 82, 155)
+insert into Okładziny values ('butterfly', 'Sriver', 2.0, 83, 155)
+insert into Okładziny values ('butterfly', 'Sriver', 2.1, 84, 155)
+insert into Okładziny values ('butterfly', 'Sriver EL', 1.9, 84, 155)
+insert into Okładziny values ('butterfly', 'Sriver EL', 2.0, 85, 155)
+insert into Okładziny values ('butterfly', 'Sriver EL', 2.1, 86, 155)
+insert into Okładziny values ('butterfly', 'Sriver FX', 1.9, 86, 155)
+insert into Okładziny values ('butterfly', 'Sriver FX', 2.0, 87, 155)
+insert into Okładziny values ('butterfly', 'Sriver FX', 2.1, 88, 155)
+insert into Okładziny values ('butterfly', 'Dignics 05', 1.9, 88, 365)
+insert into Okładziny values ('butterfly', 'Dignics 05', 2.0, 89, 365)
+insert into Okładziny values ('butterfly', 'Dignics 05', 2.1, 90, 365)
+insert into Okładziny values ('butterfly', 'Dignics 09C', 1.9, 88, 365)
+insert into Okładziny values ('butterfly', 'Dignics 09C', 2.0, 89, 365)
+insert into Okładziny values ('butterfly', 'Dignics 09C', 2.1, 90, 365)
+insert into Okładziny values ('butterfly', 'Dignics 64', 1.9, 88, 365)
+insert into Okładziny values ('butterfly', 'Dignics 64', 2.0, 89, 365)
+insert into Okładziny values ('butterfly', 'Dignics 64', 2.1, 90, 365)
+insert into Okładziny values ('butterfly', 'Dignics 80', 1.9, 88, 365)
+insert into Okładziny values ('butterfly', 'Dignics 80', 2.0, 89, 365)
+insert into Okładziny values ('butterfly', 'Dignics 80', 2.1, 90, 365)
+
+
+
+
+insert into Deski values ('Donic', 'Dyjas Ultra Power', 72, 148)
+
+
+
+insert into Okładziny ( Producent,Model,Grubość,Waga,Cena) values ('Andro', 'Hexer', 1.8, 80, 130)
+
+
+INSERT INTO Zestawy(Nazwa, Opis, Okładzina1ID, Okładzina2ID, DeskaID)
+values ('Szympek napierdalacz', 'Bardzo szybki zestaw dla prawdziwych graczy', 43, 5, 1)
+
+
+
+INSERT INTO Deski values ('Donic', 'Waldner Offensive 2016', 76, 128)
+INSERT INTO Deski values ('Donic', 'Waldner Black Devil CB', 71, 198)
+INSERT INTO Deski values ('Donic', 'Persson Power Carbon', 79, 148)
+INSERT INTO Deski values ('Donic', 'Extension OC', 70, 398)
+INSERT INTO Deski values ('Donic', 'Persson OFF+', 75, 298)
+INSERT INTO Deski values ('Donic', 'Epox Top Speed', 70, 128)
+
+
+INSERT INTO Zestawy(Nazwa, Opis, Okładzina1ID, Okładzina2ID, DeskaID)
+values ('PawelWola', 'BardZo wolny zestaw dla początkujących amatorów', 25, 26, 7)
+
+

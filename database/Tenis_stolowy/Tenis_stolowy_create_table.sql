173449452f29433e137b4bd0d386938fc31347c6
szymon kolodziej <szymonk474@gmail.com>
2021-06-08 13:47:31 +0200

    Poprawki

--- database/Tenis_stolowy/Tenis_stolowy_create_table.sql
+++ database/Tenis_stolowy/Tenis_stolowy_create_table.sql
@@ -1,34 +1,34 @@
 CREATE TABLE [dbo].[Deski](
+	[DeskiID] [int] IDENTITY(1,1) NOT NULL,
 	[Producent] [nvarchar](20) NULL,
 	[Model] [nvarchar](60) NULL,
 	[Waga] [tinyint] NULL,
 	[Cena] [float] NULL,
-	[DeskiID] [int] IDENTITY(1,1) NOT NULL,
 	[Opinie] [varchar](50) NULL,
- CONSTRAINT [pk_Deski_DeskiID] PRIMARY KEY CLUSTERED 
-
+ CONSTRAINT [pk_Deski_DeskiID] PRIMARY KEY (DeskiID)
+ )
 GO
 
 
-CREATE TABLE [dbo].[Okładziny](
+CREATE TABLE [dbo].[Okladziny](
+	[OkladzinyID] [int] IDENTITY(1,1) NOT NULL,
 	[Producent] [nvarchar](max) NULL,
 	[Model] [nvarchar](max) NULL,
-	[Grubość] [float] NULL,
+	[Grubosc] [float] NULL,
 	[Waga] [decimal](18, 0) NULL,
 	[Cena] [float] NULL,
 	[Kolor] [nvarchar](max) NULL,
-	[OkładzinyID] [int] IDENTITY(1,1) NOT NULL,
- CONSTRAINT [pk_Okładziny_OkładzinyID] PRIMARY KEY CLUSTERED 
-
+ CONSTRAINT [pk_Okladziny_OkladzinyID] PRIMARY KEY (OkladzinyID) 
+)
 GO
 
 
-CREATE TABLE [dbo].[Piłeczki](
+CREATE TABLE [dbo].[Pileczki](
 	[Rodzaj] [varchar](20) NULL,
 	[Producent] [varchar](20) NULL,
 	[Model] [varchar](20) NULL,
 	[Cena] [varchar](10) NULL
-) ON [PRIMARY]
+) 
 GO
 
 
@@ -36,11 +36,11 @@ CREATE TABLE [dbo].[Zestawy](
 	[ZestawyID] [int] IDENTITY(1,1) NOT NULL,
 	[Nazwa] [nvarchar](30) NULL,
 	[Opis] [nvarchar](100) NULL,
-	[Okładzina1ID] [int] NULL,
-	[Okładzina2ID] [int] NULL,
+	[Okladzina1ID] [int] NULL,
+	[Okladzina2ID] [int] NULL,
 	[DeskaID] [int] NULL,
-PRIMARY KEY CLUSTERED 
-
+ CONSTRAINT [pk_Zestawy_ZestawyID] PRIMARY KEY (ZestawyID) 
+)
 	
 GO
 
